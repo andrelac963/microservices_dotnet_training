e7a71cb90d06b480d22ceda4fa518c4892937bd4
André Luiz Alves Costa <andrelac963@gmail.com>
2023-08-15 19:54:55 -0300

    feat: implementando crud básico de uma pessoa com requisições rest

---
+++ 02_RestWithASPNET_Persons/02_RestWithASPNET_Persons/02_RestWithASPNET_Persons/Program.cs
@@ -0,0 +1,22 @@
+using _02_RestWithASPNET_Persons.Services;
+using _02_RestWithASPNET_Persons.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ 02_RestWithASPNET_Persons/02_RestWithASPNET_Persons/02_RestWithASPNET_Persons/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using _02_RestWithASPNET_Persons.Model;
+
+namespace _02_RestWithASPNET_Persons.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
